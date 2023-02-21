bf5330c096abeac8d643ae125e18cb8a62aee6b6
Armon <armon@nudgex.net>
2023-02-21 02:19:44 -0800

    actually fixed the negative-width bug this time

--- nn_visualizer.py
+++ nn_visualizer.py
@@ -11,7 +11,7 @@ class Line:
     """
     def __init__(self,x1,y1,x2,y2,width):
         color = 'green' if width > 0 else 'red'
-        self.svg = f'<line x1="{x1}" y1="{y1}" x2="{x2}" y2="{y2}" stroke="{color}" stroke-width="{width}"/>'
+        self.svg = f'<line x1="{x1}" y1="{y1}" x2="{x2}" y2="{y2}" stroke="{color}" stroke-width="{abs(width)}"/>'
 
 
 class NNVis:
@@ -78,7 +78,7 @@ class NNVis:
             for y1 in y_list_1:
                 weight_row = next(row_iter)
                 for y2,weightval in zip(y_list_2, weight_row):
-                    line_width = abs(weightval / maxweight)
+                    line_width = weightval / maxweight
                     lines.append(Line(x1,y1,x2,y2,line_width))
             x1 = x2
             y_list_1 = y_list_2
